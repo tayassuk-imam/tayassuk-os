33fa000902f1dbd3277d8e007693ea4a5343520b
tayassuk-imam <tayassuk101@gmail.com>
2026-09-04 09:49:52 +0600

    Add files via upload

---
+++ content/education.js
@@ -0,0 +1,20 @@
+export const education = [
+  {
+    institution: 'Daffodil International University',
+    degree: 'B.Sc. in Software Engineering',
+    status: 'Present',
+    details: 'Current Semester: 4th | CGPA: 3.71 / 4.00'
+  },
+  {
+    institution: 'Higher Secondary Certificate (HSC)',
+    degree: 'Science',
+    status: '2023',
+    details: 'GPA: 5.00'
+  },
+  {
+    institution: 'Secondary School Certificate (SSC)',
+    degree: 'Science',
+    status: '2021',
+    details: 'GPA: 5.00'
+  }
+];
